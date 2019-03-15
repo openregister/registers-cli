d088e8068156b9bf89eff5f1ca727b348cda605f
Arnau Siches <arnau.siches@digital.cabinet-office.gov.uk>
2019-03-15 10:21:29 +0000

    feat: Minimal CLI scaffold
    Signed-off-by: Arnau Siches <arnau.siches@digital.cabinet-office.gov.uk>

---
+++ registers/cli.py
@@ -0,0 +1,28 @@
+import click
+import os.path
+
+from .core import EMPTY_ROOT_HASH, __version__
+
+
+@click.group()
+@click.version_option(prog_name=click.style("registers", fg='bright_yellow'),
+                      version=__version__)
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("filepath", type=click.Path(exists=False))
+def init(filepath):
+    """Creates an empty register."""
+    assert_root_hash = "assert-root-hash\t{}".format(EMPTY_ROOT_HASH)
+
+    if os.path.exists(filepath):
+        raise click.UsageError("Please select a file that does not exist.")
+
+    with click.open_file(filepath, "w", "utf-8") as handle:
+        handle.write(assert_root_hash)
+
+    click.echo("Empty register successfully created at {}"
+               .format(click.style(click.format_filename(filepath),
+                                   fg='bright_yellow')))
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='registers',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        registers=registers.cli:cli
+    ''',
+)
