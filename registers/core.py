be107117fa4a808cde8cebbd2b755769a64262db
Arnau Siches <arnau.siches@digital.cabinet-office.gov.uk>
2019-03-23 11:17:20 +0000

    docs: Document core module
    Signed-off-by: Arnau Siches <arnau.siches@digital.cabinet-office.gov.uk>

--- registers/core.py
+++ registers/core.py
@@ -1,13 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module implements core functions.
+
+:copyright: © 2019 Crown Copyright (Government Digital Service)
+:license: MIT, see LICENSE for more details.
+"""
+
 EMPTY_ROOT_HASH = "sha-256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" # NOQA
 SHA256_PREFIX = "sha-256"
 
 __version__ = "0.1.0"
 
 
-def format_timestamp(dt):
+def format_timestamp(datetime):
     """
     Formats a UTC datetime as a RFC3339 timestamp.
 
     Warning: UTC timezone is assumed.
     """
-    return f"{dt.isoformat(timespec='seconds')}Z"
+
+    return f"{datetime.isoformat(timespec='seconds')}Z"
